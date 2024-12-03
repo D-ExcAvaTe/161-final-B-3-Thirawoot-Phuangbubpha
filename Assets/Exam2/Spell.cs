e26509ddc63f78de65cd0bcb430f62d10ab47ef1
D-ExcAvaTe <thirawoot.phua@bumail.net>
2024-12-03 15:30:08 +0700

    I draw a birb and a dog

---
+++ Assets/Exam2/Spell.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spell : MonoBehaviour
+{
+    public virtual void Cast()
+    {
+
+    }
+    public void Cast(string target)
+    {
+
+    }
+    public void Cast(string target,int powerLevel)
+    {
+
+    }
+}
